1c014ba8530c27fda71957420813be36b71bf2f2
Eric <ealbor@my.wctc.edu>
2020-10-25 22:16:58 -0500

    Completed Module 3

---
+++ src/app/product.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+
+import { catchError, tap, map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { Http, Response} from '@angular/http';
+
+
+@Injectable()
+export class ProductService {
+  private _albumUrl: string = "../assets/album.json";
+
+  constructor(private _http: Http){}
+
+  //By default, a method may return anything because it can infer that it is going to be of type Object<any>
+  getAlbum(id: number)  {
+    
+    return this._http.get(this._albumUrl).pipe(
+      map((response:Response) => response.json()) 
+
+      )
+    }
+
+
+}
