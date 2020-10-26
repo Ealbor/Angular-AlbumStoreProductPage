4a07e28004a210d6b1f28dd3ca70abc1fac3fcdb
Eric <ealbor@my.wctc.edu>
2020-10-26 00:20:58 -0500

    Another Attempt
    This should be accurate. No errors in console, and the test results do not make any sense

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,25 +1,22 @@
-import { Injectable } from '@angular/core';
-import {  map } from 'rxjs/operators';
-import { Http, Response} from '@angular/http';
-import 'rxjs/add/operator/map';
-import { Observable } from 'rxjs';
-
-import { Album } from './album';
+import { Injectable } from "@angular/core";
+import { map } from "rxjs/operators";
+import { Http, Response } from "@angular/http";
+import "rxjs/add/operator/map";
 
 
+import { Album } from "./album";
+import { Observable } from "rxjs/Observable";
 
 @Injectable()
 export class ProductService {
   private _albumUrl: string = "../assets/album.json";
 
-  constructor(private _http: Http){}
+  constructor(private _http: Http) {}
 
   //By default, a method may return anything because it can infer that it is going to be of type Object<any>
-  getAlbum(id: number): Observable<Album>  {
-    
-    return this._http.get(this._albumUrl).map((response) => <Album>response.json()
-      )
-    }
-
-
+  getAlbum(id: number): Observable<Album> {
+    return this._http
+      .get(this._albumUrl)
+      .map((response) => <Album>response.json());
+  }
 }
