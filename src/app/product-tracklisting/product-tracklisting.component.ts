c29846fdfe5cfafbaf5313c28328350f896ef456
Eric <ealbor@my.wctc.edu>
2020-10-26 21:00:25 -0500

    Once again

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -1,26 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { Http } from '@angular/http';
-import { ProductService } from '../product.service';
+import { Component, OnInit } from "@angular/core";
 
-import {Album} from "../album";
+import { ProductService } from "../product.service";
+import { Album } from "../album";
 
 
 @Component({
-  selector: 'app-product-tracklisting',
-  templateUrl: './product-tracklisting.component.html',
-  styleUrls: ['./product-tracklisting.component.css']
+  selector: "app-product-tracklisting",
+  templateUrl: "./product-tracklisting.component.html",
+  styleUrls: ["./product-tracklisting.component.css"],
 })
 export class ProductTracklistingComponent implements OnInit {
-
   albumInfo: Album;
-  
 
-  constructor(private _productService:ProductService) { }
+  constructor(private _productService: ProductService) {}
 
-  ngOnInit() 
-  {
-    this._productService.getAlbum(1).subscribe((response => this.albumInfo = response));
-    
+  ngOnInit() {
+    this._productService
+      .getAlbum(1)
+      .subscribe((response => this.albumInfo = response));
   }
-
 }
